9a68ec239d53b4a9a425e35b98dd77edeea18b4d
Joshua Calma <calmajoshuaf@gmail.com>
2026-01-30 12:57:35 -0800

    1/30
    - home-students ui frontend fetch

---
+++ frontend/src/js/home-student.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const userId = localStorage.getItem("userId");
+    
+    studentHomeLoad(`http://localhost:5000/api/students/globalLeaderboard`, userId);
+});
+
+async function studentHomeLoad(url, userId) {
+
+
+    try {
+        const res = await fetch(url);
+        const students = await res.json();
+
+        const activitiesCount = document.getElementById("activitiesCount");
+        const overallPointsCount = document.getElementById("overallPointsCount");
+        const rankCount = document.getElementById("rankCount");
+
+        const me = students.find(s => s._id === userId);
+
+        console.log("ACTIVITIES COUNT:", me.completedActivities)
+
+        activitiesCount.textContent = me?.completedActivities || 0;
+        overallPointsCount.textContent = me?.totalPoints || 0;
+        const myIndex = students.findIndex(s => s._id === userId);
+        rankCount.textContent = myIndex >= 0 ? myIndex + 1 : "N/A";
+
+    } catch (err) {
+        console.error("Failed to load leaderboard", err);
+    }
+}
