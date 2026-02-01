d84feb81bd19d5c0896a483bb7de9fd9a9f328b5
Joshua Calma <calmajoshuaf@gmail.com>
2026-02-01 15:08:20 -0800

    2/1
    - adjustments in index page

--- frontend/src/js/index.js
+++ frontend/src/js/index.js
@@ -1,7 +1,13 @@
-const username = document.getElementById('username');
-const password = document.getElementById('password');
-const loginButton = document.getElementById('login-button');
-const errorMessage = document.getElementById('errorMessage');
+const form = document.getElementById("login-form");
+const username = document.getElementById("username");
+const password = document.getElementById("password");
+const errorMessage = document.getElementById("errorMessage");
+
+form.addEventListener("submit", async (e) => {
+  e.preventDefault(); // prevent page reload
+  await loginUser();
+});
+
 
 if(localStorage.getItem('username') !== null && localStorage.getItem('role') === 'student') {
 
@@ -13,15 +19,10 @@ if(localStorage.getItem('username') !== null && localStorage.getItem('role') ===
 
 }
 
-loginButton.addEventListener('click', async () => {
-
-
-  console.log('Username:', username.value);
-  console.log('Password:', password.value);
-  
-  const res = await fetch('http://localhost:5000/api/login', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
+async function loginUser() {
+  const res = await fetch("http://localhost:5000/api/login", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
     body: JSON.stringify({
       username: username.value,
       password: password.value
@@ -29,20 +30,21 @@ loginButton.addEventListener('click', async () => {
   });
 
   const data = await res.json();
-  console.log(data);
 
-  if(res.ok) {
-    localStorage.setItem('username', data.user.username);
+  if (res.ok) {
+    localStorage.setItem("username", data.user.username);
     localStorage.setItem("firstName", data.user.firstName);
     localStorage.setItem("lastName", data.user.lastName);
-    localStorage.setItem('role', data.user.role);
-    localStorage.setItem('userId', data.user._id);
-   if(localStorage.getItem('role') === 'student') {
-      window.location.href = '/frontend/public/home-student.html';
-    } else if(localStorage.getItem('role') === 'instructor') {
-      window.location.href = '/frontend/public/home-instructor.html';
-    }
+    localStorage.setItem("role", data.user.role);
+    localStorage.setItem("userId", data.user._id);
+
+    window.location.href =
+      data.user.role === "student"
+        ? "/frontend/public/home-student.html"
+        : "/frontend/public/home-instructor.html";
   } else {
     errorMessage.textContent = data.message;
   }
-});
+}
+
+
